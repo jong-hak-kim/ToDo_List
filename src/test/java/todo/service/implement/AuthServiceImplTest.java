ab9932043c4420259d35472719ee22ad51f0bba3
Jong Hak Kim <whdgkr9070@naver.com>
2024-09-05 12:49:56 +0900

    test: Add duplicate email test & duplicate phone number test

--- src/test/java/todo/service/implement/AuthServiceImplTest.java
+++ src/test/java/todo/service/implement/AuthServiceImplTest.java
@@ -3,9 +3,14 @@ package todo.service.implement;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import todo.common.constant.ResponseMessage;
+import todo.dto.request.SignUpRequestDto;
+import todo.dto.response.ResponseDto;
 import todo.util.PasswordUtil;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -17,6 +22,46 @@ class AuthServiceImplTest {
 
     @Autowired
     private PasswordUtil passwordUtil;
+
+
+    @Test
+    void addDuplicateEmail() throws Exception {
+        //given
+        SignUpRequestDto dto = new SignUpRequestDto();
+        dto.setEmail("user1@naver.com");
+        dto.setPassword("userPass2");
+        dto.setProfileImg("profile.jpg");
+        dto.setPhoneNumber("010-1234-5678");
+
+        //when
+        ResponseEntity<ResponseDto> responseEntity = authServiceImpl.userSignUp(dto);
+        ResponseEntity<ResponseDto> existUserEmail = ResponseMessage.EXIST_USER_EMAIL;
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(existUserEmail.getStatusCode());
+        assertThat(responseEntity.getBody().getCode()).isEqualTo(existUserEmail.getBody().getCode());
+        assertThat(responseEntity.getBody().getMessage()).isEqualTo(existUserEmail.getBody().getMessage());
+
+    }
+
+    @Test
+    void addDuplicatePhoneNumber() throws Exception {
+        //given
+        SignUpRequestDto dto = new SignUpRequestDto();
+        dto.setEmail("newUser@naver.com");
+        dto.setPassword("userPass2");
+        dto.setProfileImg("profile.jpg");
+        dto.setPhoneNumber("010-1111-1111");
+        //when
+        ResponseEntity<ResponseDto> responseEntity = authServiceImpl.userSignUp(dto);
+        ResponseEntity<ResponseDto> existPhoneNumber = ResponseMessage.EXIST_PHONE_NUMBER;
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(existPhoneNumber.getStatusCode());
+        assertThat(responseEntity.getBody().getCode()).isEqualTo(existPhoneNumber.getBody().getCode());
+        assertThat(responseEntity.getBody().getMessage()).isEqualTo(existPhoneNumber.getBody().getMessage());
+    }
+
 
     @Test
     void passwordEncode() throws Exception {
