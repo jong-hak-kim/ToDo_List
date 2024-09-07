22288f55d53cfe1e5e4758875adea4105859b487
Jong Hak Kim <whdgkr9070@naver.com>
2024-09-07 15:42:38 +0900

    test: Add Jwt Token generate & user information payload Test

--- src/test/java/todo/util/JwtTokenUtilTest.java
+++ src/test/java/todo/util/JwtTokenUtilTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package todo.util;
+
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class JwtTokenUtilTest {
-  
+
+    @Test
+    void generateTokenTest() throws Exception {
+
+        //given
+        String token = JwtTokenUtil.generateToken("whdgkr9070@naver.com", "user");
+        Claims claimsFromToken = JwtTokenUtil.getClaimsFromToken(token);
+        //when
+
+        //then
+        assertThat(token).isNotNull();
+        assertThat(claimsFromToken)
+                .containsEntry("email", "whdgkr9070@naver.com")
+                .containsEntry("role", "user");
+    }
+
 }
